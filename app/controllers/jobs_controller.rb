d486ba483964b0761451671c1598aba9852b0e7c
nathanye <khalil54781@gmail.com>
2018-08-24 21:34:49 +0800

    implement step 5

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,6 +7,11 @@ class JobsController < ApplicationController
 
   def show
     @job = Job.find(params[:id])
+
+    if @job.is_hidden
+      flash[:warning] = "这个职位不可见"
+      redirect_to root_path
+    end
   end
 
   def edit
